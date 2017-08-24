b3eba3c841847064662dfc11dbb08951be11581a
Judea Dumont <31213205+JudeaDumont@users.noreply.github.com>
2017-08-23 22:16:47 -0600

    SinglyLinkedListFileReplaces LinkedList

--- LinkedList.java
+++
@@ -1,8 +0,0 @@
-package cs230individualproject;
-
-public interface LinkedList <Item> extends Iterable<Item>{
-    void insertAfter(Item item);
-    void deleteNext();
-    boolean hasNext();
-    Item next();
-}
